bc0ac7e02afee28b03d99e3b69031a2864280f13
g091 <gotieno791@gmail.com>
2022-04-19 20:49:11 +0300

    main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void _puts(char *str);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _strlen(const char *s);
+
+#endif
